a1adf50d86ec3b822ab07384fceedda2280502f3
IdeFFiX <ideffix1337@gmail.com>
2023-02-19 14:38:40 +0100

    connect backend and frontend

--- backend/src/main/java/com/moviefy/backend/controller/AppController.java
+++ backend/src/main/java/com/moviefy/backend/controller/AppController.java
@@ -2,9 +2,11 @@ package com.moviefy.backend.controller;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/config")
 public class AppController {
 
     @Value("${app.version}")
@@ -13,12 +15,12 @@ public class AppController {
     @Value("${app.name}")
     private String name;
 
-    @GetMapping("/config/name")
+    @GetMapping("/name")
     public String getAppName() {
         return name;
     }
 
-    @GetMapping("/config/version")
+    @GetMapping("/version")
     public String getVersion() {
         return version;
     }
--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,5 +1,29 @@
-export default function Home() {
+import {GetServerSideProps} from "next";
+
+interface ServerProps {
+  name: string;
+  version: string;
+}
+
+export const getServerSideProps: GetServerSideProps<ServerProps> = async (context) => {
+  const namePromise = fetch("http://localhost:8080/config/name").then(res => res.text());
+  const versionPromise = fetch("http://localhost:8080/config/version").then(res => res.text());
+
+  const promises = await Promise.all([namePromise, versionPromise]);
+
+  return {
+    props: {
+      name: promises[0] as string,
+      version: promises[1] as string
+    }
+  }
+};
+
+export default function Home({name, version}: ServerProps) {
   return (
-    <h1>moviefy</h1>
+    <>
+      <h1>App name: {name}</h1>
+      <h2>Version: {version}</h2>
+    </>
   )
 }
