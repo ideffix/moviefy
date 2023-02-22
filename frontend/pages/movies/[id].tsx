2d338a0d6fe55828958545bc073858637b6ab489
IdeFFiX <ideffix1337@gmail.com>
2023-02-22 22:54:55 +0100

    add movie view

--- frontend/pages/movies/[id].tsx
+++ frontend/pages/movies/[id].tsx
@@ -1,4 +1,6 @@
 import {GetServerSideProps} from "next";
+import styles from "./Movies.module.css";
+import Gallery from "react-photo-gallery";
 
 interface ServerProps {
   movie: Movie;
@@ -40,7 +42,31 @@ export const getServerSideProps: GetServerSideProps<ServerProps> = async (contex
 
 function MoviePage({movie}: ServerProps) {
 
-  return <div><h1>{movie.title}</h1></div>
+  return <div className={styles.container}>
+    <h1 className={styles.title}>{movie.title}</h1>
+    <div className={styles.info}>
+      <div className={styles.poster}>
+        <img src={movie.poster} alt={'movie poster'}/>
+      </div>
+      <div className={styles.additionalInfo}>
+        <div>
+          Here should be some description but this is missing in a backend.
+        </div>
+        <div className={styles.gridContainer}>
+          <div className={styles.firstCol}>Director</div><div>{movie.director}</div>
+          <div className={styles.firstCol}>Scenario</div><div>{movie.scenario}</div>
+          <div className={styles.firstCol}>Genre</div><div>{movie.genre.map(el => el.toLowerCase().replaceAll("_", " ")).join(', ')}</div>
+          <div className={styles.firstCol}>Production</div><div>{movie.production} this is wrong - it should be country like USA</div>
+          <div className={styles.firstCol}>Premiere</div><div>{movie.premiere} this is wrong - it should be a date</div>
+          <div className={styles.firstCol}>Awards</div><div>{movie.awards.map(el => el.toLowerCase().replaceAll("_", " ")).join(', ')}</div>
+        </div>
+      </div>
+    </div>
+    <div>
+      <h2>Gallery</h2>
+      <Gallery photos={movie.moviePhotos.map(src => ({src , width: 1, height: 1}))} />
+    </div>
+  </div>
 }
 
 export default MoviePage;
